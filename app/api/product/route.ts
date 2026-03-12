3bcfa120b5bc7564a85d38daeb37eeacd85ef3ca
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-12 19:53:54 +0530

    product image is upload on cloudinary

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,41 +2,38 @@ import { connectDB } from "@/app/lib/db";
 import serverCatchError from "@/app/lib/server-catch-error";
 import { NextRequest, NextResponse as res } from "next/server";
 import ProductModel from "@/app/models/product.model";
-import path from "path";
-import fs from "fs"
-import {v4 as uuid} from "uuid"
+import cloudinary from "@/app/lib/cloudinary";
 
 export const POST = async (req: NextRequest) => {
-    try {
-        await connectDB();
-        const body = await req.formData()
-        const file = body.get('image') as File | null
+  try {
+    await connectDB();
+    const body = await req.formData();
+    const file = body.get("image") as File | null;
 
-        if(!file)
-            return res.json({message: "Product image not sent"}, {status: 400})
+    if (!file)
+      return res.json({ message: "Product image not sent" }, { status: 400 });
 
-        const bytes = await file.arrayBuffer()
-        const buffer = Buffer.from(bytes)
-        const root = process.cwd()
-        const folder = path.join(root, "public", "products")
-        const fileName = `${uuid()}.png`
-        const filePath = path.join(folder, fileName)
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    const base64 = `data:${file.type};base64,${buffer.toString("base64")}`;
 
-        fs.writeFileSync(filePath, buffer)
+    const uploaded = await cloudinary.uploader.upload(base64, {
+      folder: "elitemart/products",
+    });
 
-        const payload = {
-            title: body.get("title"),
-            description: body.get("description"),
-            price: body.get("price"),
-            discount: body.get("discount"),
-            image: `/products/${fileName}`,
-        }
+    const payload = {
+      title: body.get("title"),
+      description: body.get("description"),
+      price: body.get("price"),
+      discount: body.get("discount"),
+      image: uploaded.secure_url,
+    };
 
-        const product = await ProductModel.create(payload)
-        return res.json(product)
-    } catch (err) {
-        return serverCatchError(err);
-    }
+    const product = await ProductModel.create(payload);
+    return res.json(product);
+  } catch (err) {
+    return serverCatchError(err);
+  }
 };
 
 export const GET = async (req: NextRequest) => {
